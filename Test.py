ef82ed7f31a135d1f5e984dbefa42cc0354d3831
18031121071 <18031121071@189.cn>
2018-01-26 17:37:56 +0800

    重新commit，解决Test.py被track不能ignore的问题

--- Test.py
+++ Test.py
@@ -2,28 +2,7 @@
 import multiprocessing
 import time
 
-def proxy(cls_instance, i):
-    return cls_instance.func(i)
+aList = [123, 'xyz', 'zara', 'abc'];
 
-class Klass(object):
-    def __init__(self):
-        print ("Constructor ... %s" % multiprocessing.current_process().name)
-
-    def __del__(self):
-        print ("... Destructor %s" % multiprocessing.current_process().name)
-
-    def func(self, x):
-        print(x * x)
-
-    def run(self):
-        pool = multiprocessing.Pool(processes=3)
-        for num in range(8):
-            #pool.apply_async(self.func, args=(num,))
-            pool.apply_async(proxy, args=(self, num,))
-            time.sleep(5)
-        pool.close()
-        pool.join()
-
-if __name__ == '__main__':
-    _kls = Klass()
-    _kls.run()
+print ("Index for xyz : ", aList.index( 'xyzf' ) )
+print ("Index for zara : ", aList.index( 'zara' ))

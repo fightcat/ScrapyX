894fd16b87305a712c238afa73a6465a547aa7a4
18031121071 <18031121071@189.cn>
2018-02-01 11:57:30 +0800

    删除Test.py测试源码

--- Test.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-import multiprocessing
-import time
-
-aList = [123, 'xyz', 'zara', 'abc'];
-
-print ("Index for xyz : ", aList.index( 'xyzf' ) )
-print ("Index for zara : ", aList.index( 'zara' ))
